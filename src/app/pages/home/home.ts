f19b014475fd013d615d6efb0ee19c4ab4b598c0
Alexfery <feralex523@gmail.com>
2026-04-03 11:50:55 +0300

    prima versiune buna, dar blocaj refresh pe skills

---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Hero } from './components/hero/hero';
+import { Skills } from './components/skills/skills';
+import { Projects } from './components/projects/projects';
+import { Contact } from './components/contact/contact';
+
+@Component({
+  selector: 'app-home',
+  imports: [Hero, Skills, Projects, Contact],
+  templateUrl: './home.html',
+  styleUrl: './home.scss'
+})
+export class Home {
+ ngAfterViewInit() {
+  setTimeout(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+        } else {
+          entry.target.classList.remove('visible');
+        }
+      });
+    }, { threshold: 0.1 });
+
+    document.querySelectorAll('.fade-in').forEach(el => {
+      observer.observe(el);
+    });
+  }, 100);
+}
+}
