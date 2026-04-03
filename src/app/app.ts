5ea1db07ff9ed2b6ff0865b5344ba2499d51cac0
Alexfery <feralex523@gmail.com>
2026-04-03 12:14:01 +0300

    prima versiune buna

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Footer } from './components/footer/footer';
 import { Navbar } from './components/navbar/navbar';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit{
+  constructor(private router: Router) {}
   ngOnInit() {
     window.scrollTo(0, 0);
-  }
+    this.router.navigate(['/'], { replaceUrl: true });
 }
-
+}
