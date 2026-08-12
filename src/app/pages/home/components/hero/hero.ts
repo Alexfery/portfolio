c1a8d28a9d09c3579d6592a84bb3c95bd330b159
Fer Alexandru <90989869+Alexfery@users.noreply.github.com>
2026-08-12 21:33:43 +0300

    stil nou

--- src/app/pages/home/components/hero/hero.ts
+++ src/app/pages/home/components/hero/hero.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, signal } from '@angular/core';
 import { LanguageService } from '../../../../services/language';
+
+const NAME = 'Fer Alexandru Virgil';
+
 @Component({
   selector: 'app-hero',
   imports: [],
   templateUrl: './hero.html',
   styleUrl: './hero.scss'
 })
-export class Hero {
-    constructor(public langService: LanguageService) {}
+export class Hero implements OnInit, OnDestroy {
+  typed = signal('');
+  private timer?: ReturnType<typeof setInterval>;
+
+  constructor(public langService: LanguageService) {}
+
+  ngOnInit() {
+    let i = 0;
+    this.timer = setInterval(() => {
+      i++;
+      this.typed.set(NAME.slice(0, i));
+      if (i >= NAME.length) clearInterval(this.timer);
+    }, 55);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.timer);
+  }
 }
